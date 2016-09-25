bfb0e27b8532402e919ecccf627b0a4fce10ce97
Damian Calabresi <damiancalabresi@gmail.com>
2016-09-25 14:58:48 -0300

    Spring Boot and Swagger integrated

--- src/main/java/com/ofg/loans/model/LoanApplication.java
+++ src/main/java/com/ofg/loans/model/LoanApplication.java
@@ -1,8 +1,20 @@
 package com.ofg.loans.model;
 
+import com.ofg.loans.configuration.SwaggerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
+
 /**
  * Loan application entity.
  */
+@SpringBootApplication
+@Import(SwaggerConfig.class)
 public class LoanApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(LoanApplication.class, args);
+    }
 
 }
